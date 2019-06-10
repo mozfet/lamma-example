69d732cd3233659d29d227c6cd44e5fbd6068a4a
Mozfet <wynand@expertbox.com>
2019-06-10 17:29:47 +0200

    remove Parrot Lama integration

--- client/main.js
+++ client/main.js
@@ -1,9 +1,5 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import { Blaze } from 'meteor/blaze'
-import '/imports/startup/client/routes.js'
+import '/imports/startup/client'
 import '/imports/ui/layouts'
-
-Meteor.startup(() => {
-  Blaze.render(Template.main_layout, document.getElementsByTagName('body')[0])
-})
--- imports/ui/layouts/main.js
+++ imports/ui/layouts/main.js
@@ -8,12 +8,26 @@ Layout.register({
     default: 'foo',
     notFound: 'notFound',
     processor: function (name) { return 'body_' + name },
-    Parrot: true
+    // Parrot: true
   },
   side: {
     default: 'foo',
     notFound: 'notFound',
     processor: function (name) { return 'side_' + name },
-    Parrot: true
+    // Parrot: true
+  }
+})
+
+Template.main_layout.events({
+  'click #js-foo-button'(event) {
+    const instance = Template.instance()
+    console.log('foo button click')
+    Layout.set({body: 'foo', side: 'foo'})
+  },
+
+  'click #js-bar-button'(event) {
+    const instance = Template.instance()
+    console.log('bar button click')
+    Layout.set({body: 'bar', side: 'bar'})
   }
 })
